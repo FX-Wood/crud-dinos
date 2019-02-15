72c27786d43fec366ac2aba53f989619f98e4623
FX-Wood <fxwood2@gmail.com>
2019-02-15 11:42:25 -0800

    added style page and it is working'

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const fs = require('fs');
 
 app.set('view engine', 'ejs');
 
+
+
+app.get('/', function(req,res) {
+    res.send('root');
+})
+
 app.use(express.static(__dirname + "/static"));
 
 app.get('/dinosaurs', function(req, res) {
-    res.send('working');
+    const dinosaurs  = fs.readFileSync('./static/lib/dinos.json');
+    var data = JSON.parse(dinosaurs)
+    res.render('dinos/index', {myDinos: data})
 })
-
 
 
 app.listen(3000);
