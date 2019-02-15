a442d29ae1d8b97565de2f72349371f6c6e95cc2
FX-Wood <fxwood2@gmail.com>
2019-02-15 11:24:18 -0800

    commit before bugfix

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/static"));
 
-app.get('/', function(req, res) {
+app.get('/dinosaurs', function(req, res) {
     res.send('working');
 })
+
+
 
 app.listen(3000);
